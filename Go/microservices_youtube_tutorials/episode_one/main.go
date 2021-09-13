813d81b00d1e829205e5225dc5a587def17112dd
Gene <jsiow002.dev@gmail.com>
2021-09-12 23:30:10 -0700

    Microservice update and refactoring

--- Go/microservices_youtube_tutorials/episode_one/main.go
+++ Go/microservices_youtube_tutorials/episode_one/main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World")
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", sm)
 }
